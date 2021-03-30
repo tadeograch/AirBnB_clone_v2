4dcf25860f7bc5eab66ffe8e5d1beb4684991c67
Tadeo Grach <tade.g.17@gmail.com>
2021-03-30 15:31:44 +0000

    fixing file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,7 +25,7 @@ class DBStorage:
                                               getenv("HBNB_MYSQL_PWD"),
                                               getenv("HBNB_MYSQL_HOST"),
                                               getenv("HBNB_MYSQL_DB")),
-                                      pool_pre_ping=True)
+                                       pool_pre_ping=True)
         if getenv("HBNB_ENV") == 'test':
             Base.metadata.drop_all(self.__engine)
         Session = sessionmaker(bind=self.__engine)
