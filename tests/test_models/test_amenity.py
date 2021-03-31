d95f687d342cc4b87664696991f6d968e1e88158
tomi1710 <2294@holbertonschool.com>
2021-03-31 00:57:53 +0000

    add more tests amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,69 @@
 #!/usr/bin/python3
-""" Test file for Amenity"""
-from tests.test_models.test_base_model import test_basemodel
+""" testing """
+import unittest
+import os
 from models.amenity import Amenity
+from models.base_model import BaseModel
+import pep8
 
 
-class test_Amenity(test_basemodel):
-    """ Testing amenity """
+class test_amenity_two(unittest.TestCase):
+    """  xdxdxd  """
+    @classmethod
+    def setUpClass(cls):
+        """  xdxdxd  """
+        cls.amenity = Amenity()
+        cls.amenity.name = "Breakfast"
 
-    def __init__(self, *args, **kwargs):
-        """ Instantation """
-        super().__init__(*args, **kwargs)
-        self.name = "Amenity"
-        self.value = Amenity
+    @classmethod
+    def teardown(cls):
+        """  xdxdxd  """
+        del cls.amenity
 
-    def test_name2(self):
-        """ Testing name """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+    def tearDown(self):
+        """  xdxdxd  """
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
+
+    def test_pep8_Amenity(self):
+        """"  xdxdxd  """
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/amenity.py'])
+        self.assertEqual(p.total_errors, 0, "pep8 error")
+
+    @unittest.skipIf(
+        os.getenv('HBNB_TYPE_STORAGE') == 'db',
+        "Filestorage")
+    def test_save_Amenity(self):
+        """  xdxdxd  """
+        self.amenity.save()
+        self.assertNotEqual(self.amenity.created_at, self.amenity.updated_at)
+
+    def test_to_dict_Amenity(self):
+        """  xdxdxd  """
+        self.assertEqual('to_dict' in dir(self.amenity), True)
+
+
+    def test_checking_for_docstring_Amenity(self):
+        """  xdxdxd  """
+        self.assertIsNotNone(Amenity.__doc__)
+
+    def test_attributes_Amenity(self):
+        """  xdxdxd  """
+        self.assertTrue('id' in self.amenity.__dict__)
+        self.assertTrue('created_at' in self.amenity.__dict__)
+        self.assertTrue('updated_at' in self.amenity.__dict__)
+        self.assertTrue('name' in self.amenity.__dict__)
+
+    def test_is_subclass_Amenity(self):
+        """  xdxdxd  """
+        self.assertTrue(issubclass(self.amenity.__class__, BaseModel), True)
+
+    def test_attribute_types_Amenity(self):
+        """  xdxdxd  """
+        self.assertEqual(type(self.amenity.name), str)
+
+if __name__ == "__main__":
+    unittest.main()
