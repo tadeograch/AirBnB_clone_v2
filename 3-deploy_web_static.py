33c144b07b629186c3d18ea530f182d90446a7e7
Tadeo Grach <tade.g.17@gmail.com>
2021-04-15 16:56:19 +0000

    fixing task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,10 +5,7 @@ from fabric.api import *
 from datetime import datetime
 from os import path
 
-env.hosts = [
-    '34.75.147.95',
-    '104.196.199.220'
-]
+env.hosts = ['34.75.147.95', '104.196.199.220']
 
 
 def do_pack():
@@ -29,8 +26,8 @@ def do_deploy(archive_path):
     if path.exists(archive_path) is False:
         return False
     try:
-        name = archive_path[9:]
-        no_extension = name[:-4]
+        name = archive_path.split("/")[1]
+        no_extension = name.split(".")[0]
         path_r = "/data/web_static/releases/"
         path_c = "/data/web_static/current"
         put(archive_path, "/tmp/")
@@ -40,7 +37,7 @@ def do_deploy(archive_path):
         run("mv {0}{1}/web_static/* {0}{1}/".format(path_r, no_extension))
         run("rm -rf {}{}/web_static".format(path_r, no_extension))
         run("rm -rf {}".format(path_c))
-        run("ln -s {}{}/ {}".format(path_r, no_extension, path_c))
+        run("ln -sf {}{}/ {}".format(path_r, no_extension, path_c))
         return True
     except:
         return False
