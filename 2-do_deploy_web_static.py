ab578de2ee5f35d135eb00eaf860a3d352e654c5
Tadeo Grach <tade.g.17@gmail.com>
2021-04-15 04:34:13 +0000

    fixing task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,10 +3,7 @@
 from fabric.api import *
 from os import path
 
-env.hosts = [
-    '34.75.147.95',
-    '104.196.199.220'
-]
+env.hosts = ['34.75.147.95', '104.196.199.220']
 
 
 def do_deploy(archive_path):
@@ -14,18 +11,17 @@ def do_deploy(archive_path):
     if path.exists(archive_path) is False:
         return False
     try:
-        name = archive_path.split("/")[-1]
+        name = archive_path.split("/")[1]
         no_extension = name.split(".")[0]
         path_r = "/data/web_static/releases/"
-        path_c = "/data/web_static/current"
         put(archive_path, "/tmp/")
         run("mkdir -p {}{}/".format(path_r, no_extension))
         run("tar -xzf /tmp/{} -C {}{}/".format(name, path_r, no_extension))
         run("rm /tmp/{}".format(name))
         run("mv {0}{1}/web_static/* {0}{1}/".format(path_r, no_extension))
         run("rm -rf {}{}/web_static".format(path_r, no_extension))
-        run("rm -rf {}".format(path_c))
-        run("ln -s {}{}/ {}".format(path_r, no_extension, path_c))
+        run("rm -rf /data/web_static/current")
+        run("ln -sf {}{}/ /data/web_static/current".format(path_r, no_extension))
         return True
     except:
         return False
