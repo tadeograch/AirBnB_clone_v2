f970d57fb3f088921debfdc82ff2707073c2eada
Tadeo Grach <tade.g.17@gmail.com>
2021-04-30 19:47:28 +0000

    making executable and adding task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -35,6 +35,12 @@ def number_variable(n):
     '''Display n is a number only if n is an integer'''
     return "{} is a number".format(n)
 
+
+@app.route('/number_template/<int:n>)', strict_slashes=False)
+def number_variable(n):
+    '''Display a HTML page only if n is an integer'''
+    return render_template("5-number.html", n=n)
+
 if __name__ == '__main__':
     '''Listening on 0.0.0.0, port 5000'''
     app.run(host='0.0.0.0', port=5000)
