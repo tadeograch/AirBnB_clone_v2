0b2215816ff6dd9efa34eb05503b151d6b404113
Tadeo Grach <tade.g.17@gmail.com>
2021-05-01 17:09:56 +0000

    fixing task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,7 +25,7 @@ def c_variable(text):
 
 
 @app.route('/python', strict_slashes=False)
-@app.route('/python/(<text>)', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def python_variable(text="is cool"):
     '''Display Python followed by the value of the text variable'''
     new_text = text.replace("_", " ")
