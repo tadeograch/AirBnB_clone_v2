555ecb6dcab58cf8374f73329bd8c92e624327ee
Tadeo Grach <tade.g.17@gmail.com>
2021-05-01 17:28:15 +0000

    fixing task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -24,20 +24,21 @@ def c_variable(text):
     return "C {}".format(escape(new_text))
 
 
-@app.route('/python/(<text>)', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def python_variable(text="is cool"):
     '''Display Python followed by the value of the text variable'''
     new_text = text.replace("_", " ")
-    return "C {}".format(escape(new_text))
+    return "Python {}".format(new_text)
 
 
-@app.route('/number/<int:n>)', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number_variable(n):
     '''Display n is a number only if n is an integer'''
     return "{} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>)', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     '''Display a HTML page only if n is an integer'''
     return render_template("5-number.html", n=n)
